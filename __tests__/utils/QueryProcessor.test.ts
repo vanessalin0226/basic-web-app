01a704af567841c2f631c2223fbb98ffdfcd3ff7
Vanessa Lin <vanessa_lin@jtron-tech.com>
2023-09-15 14:21:01 -0400

    added andrew id query and test

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return my andrew id', () => {
+        const query = "andrew id";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "vanessa2"
+          ));
+    });
 });
